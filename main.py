f8591c19aa895b3873570c1161d909d62770e768
Myle Urdaneta <eddami16@gmail.com>
2022-07-17 11:38:27 -0500

    Agregue readchar

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import readchar
+
 nombre = input("Digite tu nombre: ")
 
 print("Bienvenid@ al proyecto", nombre)
+
+status = True
+
+while status:
+    print("Ingrese caracter: ")
+    c = readchar.readkey()
+        
+    if c == readchar.key.DOWN:
+        status = False
+    else:
+        print("Ingrese caracter: ")
+        c = readchar.readkey()
+        
