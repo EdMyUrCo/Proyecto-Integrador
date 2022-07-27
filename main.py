d05731d507a8638938841729b83febff76d05d66
Myle Urdaneta <eddami16@gmail.com>
2022-07-26 21:07:25 -0500

    Modificado

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import os
+os.system("clear")
 import readchar
 
 nombre = input("Digite tu nombre: ")
 
 print("Bienvenid@ al proyecto", nombre)
 
-status = True
+# status = True
 
-while status:
+# while status:
+#     print("Ingrese caracter: ")
+#     c = readchar.readkey()
+        
+#     if c == readchar.key.DOWN:
+#         status = False
+
+for i in range (0, 50+1):
     print("Ingrese caracter: ")
     c = readchar.readkey()
-        
-    if c == readchar.key.DOWN:
-        status = False
 
+    if c == readchar.key.CTRL_N:
+        
