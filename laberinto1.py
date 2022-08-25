8a56a99c2e9d39690790bc263dbefb24de93599b
EDDA MYLE URDANETA CORDOBA <eddami16@gmail.com>
2022-08-24 19:04:02 -0500

    Modificacin comentarios

--- laberinto1.py
+++ laberinto1.py
@@ -1,20 +1,11 @@
 def laberinto(dimension, muros):
-    # Creamos una lista vacía para añadir las filas del laberinto.
     laberinto = []
-    # Bucle iterativo para añadir las filas del laberinto.
-    # i toma valores de 0 a dimension-1
     for i in range(dimension):
-        # Creamos una lista vacía para añadir las casillas de la fila.
         fila = []
-        # Bucle iterativo para recorrer las columnas del laberinto.
-        # j toma valores de 0 a dimension-1.
         for j in range(dimension):
-            # Condicional para comprobar si la tupla está en el la lista de muros.
             if tuple([i, j]) in muros:
-                # Si la tupla está en la lista de muros ponemos una X en la casilla.
                 fila.append('#')
             else:
-                # Si la tupla no está en el muro ponemos un espacio en blanco en la casilla.
                 fila.append('.')
         laberinto.append(fila)
     return laberinto
